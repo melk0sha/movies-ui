68b9e331e4c15a3405fb18bcd928fa370d33d430
melk0sha <din.ivanova13@gmail.com>
2021-02-13 01:50:02 +0300

    feat: added webpack settings

---
+++ config/webpack.common.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "../src/index.jsx")
+  },
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "[name].bundle.js"
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Movies UI",
+      favicon: path.resolve(__dirname, "../public/favicon.ico"),
+      template: path.resolve(__dirname, "../public/template.html"),
+      filename: "index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.(ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource"
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      assets: path.resolve(__dirname, "../src/assets"),
+      components: path.resolve(__dirname, "../src/components"),
+      constants: path.resolve(__dirname, "../src/constants"),
+      containers: path.resolve(__dirname, "../src/containers"),
+      routes: path.resolve(__dirname, "../src/routes"),
+      utils: path.resolve(__dirname, "../src/utils")
+    }
+  }
+};
---
+++ config/webpack.prod.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: false,
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/",
+    filename: "js/[name].[contenthash].bundle.js"
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
+});
