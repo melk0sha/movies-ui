17c1829847113ae194c131375cf75db4524a60fe
melk0sha <din.ivanova13@gmail.com>
2021-02-18 13:35:16 +0300

    refactor: added example test file and added node-env flag to run webpack

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { GlobalStyles } from "assets/styles";
 
 const App = () => {
+  /**
+   * You will see 'development' mode in console in your browser via development starting.
+   * But you can't see 'production' mode now, since it is will be shown only on deployed version of app.
+   */
+  console.log(`process.env.NODE_ENV = ${process.env.NODE_ENV}`);
+
   return (
     <>
       <div className="wrapper">Hi</div>
