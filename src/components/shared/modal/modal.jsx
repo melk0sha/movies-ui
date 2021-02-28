7e713490850bf0766e82cb96a31a5173ae2feb2d
melk0sha <din.ivanova13@gmail.com>
2021-03-01 01:15:21 +0300

    feat: added skeleton for modal component

---
+++ src/components/shared/modal/modal.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ModalWrapper } from "components/shared/modal/modal.styled";
+
+class Modal {
+  render() {
+    return <ModalWrapper></ModalWrapper>;
+  }
+}
+
+Modal.propTypes = {};
+
+Modal.defaultProps = {};
+
+export { Modal };
