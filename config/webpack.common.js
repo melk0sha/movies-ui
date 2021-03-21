3ca731eb88a8f79b97a3759e88a65c9095a6d6b4
melk0sha <din.ivanova13@gmail.com>
2021-03-21 17:19:49 +0300

    refactor: added additional aliases

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -40,12 +40,15 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
     alias: {
+      actions: path.resolve(__dirname, "../src/actions"),
       api: path.resolve(__dirname, "../src/api"),
       assets: path.resolve(__dirname, "../src/assets"),
       components: path.resolve(__dirname, "../src/components"),
       consts: path.resolve(__dirname, "../src/constants"),
       hooks: path.resolve(__dirname, "../src/hooks"),
+      reducers: path.resolve(__dirname, "../src/reducers"),
       routes: path.resolve(__dirname, "../src/routes"),
+      store: path.resolve(__dirname, "../src/store"),
       types: path.resolve(__dirname, "../src/types"),
       utils: path.resolve(__dirname, "../src/utils")
     }
