77430d3131f5ad1e7568952b423dafc7a0d0a332
melk0sha <din.ivanova13@gmail.com>
2021-04-01 19:52:56 +0300

    refactor: installed styles and css loader to support libraries

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -34,6 +34,10 @@ module.exports = {
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
         type: "asset/inline"
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
