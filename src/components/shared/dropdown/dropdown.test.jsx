78ad3b8c09a8b53d6a503f5314e509aefc0b1a14
melk0sha <din.ivanova13@gmail.com>
2021-04-20 17:00:40 +0300

    feat: added tests for dropdown and updateMovieFields component

---
+++ src/components/shared/dropdown/dropdown.test.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { fireEvent } from "@testing-library/react";
+import { render } from "tests/helpers/render";
+import { theme } from "assets/styles/theme";
+import Dropdown from "components/shared/dropdown";
+import "@testing-library/jest-dom/extend-expect";
+
+// className,
+// options = [],
+// defaultLabel = "",
+// selectedOptions = [],
+// onSelect,
+// primary = false,
+// multiSelect = false
+
+describe("<Dropdown /> component testing", () => {
+  const options = [{ id: 1, value: "Option 1" }, { value: "Option 2" }];
+
+  it("should render a Dropdown component with initial state", () => {
+    const { container } = render(<Dropdown />, { theme });
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should handle onSelect for singleSelect and primary option", () => {
+    const handleSelect = jest.fn();
+    const labelText = "Default label";
+    const { container, getByText } = render(
+      <Dropdown primary options={options} defaultLabel={labelText} onSelect={handleSelect} />,
+      {
+        theme
+      }
+    );
+    const [firstOption] = options;
+
+    const labelElement = getByText(labelText);
+
+    fireEvent.click(labelElement);
+
+    const optionElement = getByText(firstOption.value);
+
+    fireEvent.click(optionElement);
+
+    expect(container).toMatchSnapshot();
+    expect(handleSelect).toHaveBeenCalled();
+  });
+
+  it("should handle onSelect for multiSelect and selected options", () => {
+    const handleSelect = jest.fn();
+    const selectedOptions = [...options];
+    const [firstOption] = options;
+    const labelTest = selectedOptions.map((option) => option.value).join(", ");
+    const { container, getByText } = render(
+      <Dropdown options={options} multiSelect selectedOptions={selectedOptions} onSelect={handleSelect} />,
+      {
+        theme
+      }
+    );
+
+    const labelElement = getByText(labelTest);
+
+    fireEvent.click(labelElement);
+
+    const optionElement = getByText(firstOption.value);
+
+    fireEvent.click(optionElement);
+
+    expect(container).toMatchSnapshot();
+    expect(handleSelect).toHaveBeenCalled();
+  });
+
+  it("should handle onSelect for multiSelect without selectedOptions", () => {
+    const handleSelect = jest.fn();
+    const [firstOption] = options;
+    const labelTest = "Default label";
+    const { container, getByText } = render(
+      <Dropdown options={options} multiSelect defaultLabel={labelTest} onSelect={handleSelect} />,
+      {
+        theme
+      }
+    );
+
+    const labelElement = getByText(labelTest);
+
+    fireEvent.click(labelElement);
+
+    const optionElement = getByText(firstOption.value);
+
+    fireEvent.click(optionElement);
+
+    expect(container).toMatchSnapshot();
+    expect(handleSelect).toHaveBeenCalled();
+  });
+});
